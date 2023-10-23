67e2580c6d97336d25b800122653480d22c9d434
Syarifhidayatulloh27 <hidayatullohsyarif119@gmail.com>
2023-10-24 00:10:10 +0800

    Membuat Ucapan

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFuntion() {
+  document.getElementById('gambar').src = 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/happy-birthday-1-1/sticker_2.gif?c41b322ffe08d4818c36a5ca53416d0c';
+  document.getElementById('tulisan').innerHTML = 'Selamat bertambah umur abangku. Semoga abang selalu diberikan kesehatan, keberkahan, dan murah rezeki dari Allah SWT. Aamiin.';
+}
